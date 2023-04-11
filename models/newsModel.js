2db684609862311028258b951f2f48eabe97f5a9
orangeCatDeveloper <mrlemon00@163.com>
2023-04-10 22:47:06 -0400

    revise news model

--- models/newsModel.js
+++ models/newsModel.js
@@ -3,15 +3,12 @@ const schema = mongoose.Schema(
     {
         author: {
             type: String,
-            required: true,
         },
         title: {
             type: String,
-            required: true,
         },
         description: {
             type: String,
-            required: true,
         },
         image: {
             type: String,
@@ -27,7 +24,6 @@ const schema = mongoose.Schema(
         },
         content: {
             type: String,
-            required: true,
         },
     }, { collection: 'news' });
 

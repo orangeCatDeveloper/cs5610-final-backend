504557919ac0a0a6cf481ab8a5a43a736fb805c2
calmix <rxsz123@163.com>
2023-04-15 13:14:50 -0400

    modify followingController

--- controllers/followingController.js
+++ controllers/followingController.js
@@ -1,17 +1,39 @@
 import Following from '../models/followingModel.js';
 
 const FollowingController = (app) => {
-    app.get("/api/user/:followerid/follow/:followeeid", getFollowing);
+    app.get("/api/user/:followerid/follow/:followeeid", isFollowing);
+    app.get("/api/user/:followerid/follow/", getFollowings);
+    app.get("/api/user/follow/:followeeid", getFollowers);
     app.put("/api/user/:followerid/follow/:followeeid", updateFollowing);
 }
 
-const getFollowing = async (req, res) => {
+const isFollowing = async (req, res) => {
     const { followerid, followeeid } = req.params;
     try {
         const isFollowing = await Following.findOne({ follower: followerid, followee: followeeid });
         res.status(200).json(isFollowing ? true : false);
     } catch (err) {
         res.status(500).json({ message: "Error retrieving following" });
+    }
+}
+
+const getFollowings = async (req, res) => {
+    const {followerid} = req.params;
+    try {
+        const followings = await Following.find({ follower: followerid}).populate("followee");
+        res.status(200).json(followings);
+    } catch (err) {
+        res.status(500).json({ message: "Error retrieving followings" });
+    }
+}
+
+const getFollowers = async (req, res) => {
+    const {followeeid} = req.params;
+    try {
+        const followers = await Following.find({ followee: followeeid}).populate("follower");
+        res.status(200).json(followers);
+    } catch (err) {
+        res.status(500).json({ message: "Error retrieving followings" });
     }
 }
 
